c8fe3b377827dd8cf70079b080a49755b78b17ea
jeonwoochul0515-cell <jeonwoochul0515@gmail.com>
2026-05-30 09:27:28 +0900

    feat: 영어/일본어 페이지 추가(hreflang·언어전환·다국어 sitemap) + AdSense 광고 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './admin.html'],
+  content: ['./index.html', './admin.html', './en.html', './ja.html'],
   theme: { extend: {} },
   plugins: [],
 }
